b74e0fbcce4b32edd5293b05ae1fd8ebf0e8b54f
Abed3228 <Abed3228@gmail.com>
2018-10-26 22:59:44 +0300

    Controllers- Delete and Details

--- BookList/Controllers/BooksController.cs
+++ BookList/Controllers/BooksController.cs
@@ -43,7 +43,6 @@ namespace BookList.Controllers
         }
 
         //Details : Books/Details/5
-
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -53,6 +52,57 @@ namespace BookList.Controllers
                 return NotFound();
             return View(book);
 
+        }
+        //Edit : Books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var book = await _db.Books.SingleOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+                return NotFound();
+            return View(book);
+
+        }
+
+        //POST : Books/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,Book book)
+        {
+            if (id != book.Id)
+                return NotFound();
+            if(ModelState.IsValid)
+            {
+                _db.Update(book);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+
+        //Delete : Books/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var book = await _db.Books.SingleOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+                return NotFound();
+            return View(book);
+
+        }
+
+        //POST : Books/Delete/5
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemobeBook(int? id)
+        {
+            var book = await _db.Books.SingleOrDefaultAsync(m => m.Id == id);
+            _db.Books.Remove(book);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
         protected override void Dispose(bool disposing)
         {
